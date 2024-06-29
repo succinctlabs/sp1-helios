fdc9b117b66bddfd0c4ea3d9d83a805f9cbb6d8c
Xavier DMello <xaviermdmello@gmail.com>
2024-06-28 18:14:58 -0700

    cleanup

--- program/src/main.rs
+++ program/src/main.rs
@@ -2,19 +2,9 @@
 
 #![no_main]
 sp1_zkvm::entrypoint!(main);
-// use helios_prover_primitives::types::{
-//     BLSPubKey, Bytes32, Header, SignatureBytes, SyncAggregate, SyncCommittee, Vector, U64,
-// };
 
 use helios_prover_primitives::types::ProofInputs;
 use primitives::consensus::verify_update;
-use primitives::forktypes::Forks;
-use primitives::types::{Bootstrap, Bytes32, Header, LightClientStore, SyncCommittee, Update, U64};
-use primitives::utils;
-use std::time::SystemTime;
-// use eyre::Result;
-
-// use std::sync::Arc;
 
 pub fn main() {
     let encoded_inputs = sp1_zkvm::io::read_vec();
