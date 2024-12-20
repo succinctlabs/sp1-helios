1d9c6a1078db0357d4dea6880bc576f7092942af
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-12-19 17:23:47 -0800

    add

--- script/build.rs
+++ script/build.rs
@@ -1,11 +1,11 @@
+#[allow(unused_imports)]
 use sp1_build::{build_program_with_args, BuildArgs};
 
 fn main() {
-    let args = BuildArgs {
-        tag: "v3.0.0".to_string(),
-        docker: true,
-        elf_name: "sp1-helios-docker".to_string(),
-        ..Default::default()
-    };
-    build_program_with_args("../program", args);
+    // build_program_with_args("../program", BuildArgs {
+    //     tag: "v3.0.0".to_string(),
+    //     docker: true,
+    //     elf_name: "sp1-helios-docker".to_string(),
+    //     ..Default::default()
+    // });
 }
