dcacdff2f35084a045b2e18bc04b11ce50df8b4b
leruaa <aurelien.catinon@gmail.com>
2025-02-13 02:31:15 -0800

    chore: update SP1 to 4.1.0

--- script/build.rs
+++ script/build.rs
@@ -5,7 +5,7 @@ fn main() {
     // build_program_with_args(
     //    "../program",
     //    BuildArgs {
-    //        tag: "v4.0.0".to_string(),
+    //        tag: "v4.1.0".to_string(),
     //        docker: true,
     //        elf_name: Some("sp1-helios-elf".to_string()),
     //        output_directory: Some("../elf".to_string()),
