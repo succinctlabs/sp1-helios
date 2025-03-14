43370260f774df07768c53f789423f30debe50dc
Ratan Kaliani <ratankaliani@berkeley.edu>
2025-03-14 16:10:46 -0700

    0.8.3

--- script/build.rs
+++ script/build.rs
@@ -7,6 +7,8 @@ fn main() {
     //    BuildArgs {
     //        docker: true,
     //        elf_name: Some("sp1-helios-elf".to_string()),
+    //        tag: Some("v4.1.3".to_string()),  
+    //        docker: true,
     //        output_directory: Some("../elf".to_string()),
     //        ..Default::default()
     //    },
