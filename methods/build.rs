1120097b76b9c9d576f8446f6f54ab5622d2eb1b
Wolfgang Welz <welzwo@gmail.com>
2025-03-24 20:25:19 +0100

    add deterministic builds

--- methods/build.rs
+++ methods/build.rs
@@ -1,3 +1,23 @@
+use std::{collections::HashMap, env, path::PathBuf};
+
+use risc0_build::{DockerOptionsBuilder, GuestOptionsBuilder};
+
 fn main() {
-    risc0_build::embed_methods();
+    // Builds can be made deterministic, and thereby reproducible, by using Docker to build the
+    // guest. Check the RISC0_USE_DOCKER variable and use Docker to build the guest if set.
+    println!("cargo:rerun-if-env-changed=RISC0_USE_DOCKER");
+    println!("cargo:rerun-if-changed=build.rs");
+    let manifest_dir = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
+    let mut builder = GuestOptionsBuilder::default();
+    if env::var("RISC0_USE_DOCKER").is_ok() {
+        let docker_options = DockerOptionsBuilder::default()
+            .root_dir(manifest_dir.join("../.."))
+            .build()
+            .unwrap();
+        builder.use_docker(docker_options);
+    }
+    let guest_options = builder.build().unwrap();
+
+    // Generate Rust source files for the methods crate.
+    risc0_build::embed_methods_with_options(HashMap::from([("r0vm-helios-guest", guest_options)]));
 }
