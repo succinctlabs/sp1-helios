266d21c6eb009651d37c040b2dde6235e5f1c69a
nhtyy <n@push0.dev>
2025-05-13 16:45:00 -0700

    fix: build active

--- script/build.rs
+++ script/build.rs
@@ -2,15 +2,13 @@
 use sp1_build::{build_program_with_args, BuildArgs};
 
 fn main() {
-    // build_program_with_args(
-    //    "../program",
-    //    BuildArgs {
-    //        docker: true,
-    //        elf_name: Some("sp1-helios-elf".to_string()),
-    //        tag: Some("v4.1.7".to_string()),
-    //        docker: true,
-    //        output_directory: Some("../elf".to_string()),
-    //        ..Default::default()
-    //    },
-    //);
+    build_program_with_args(
+       "../program",
+       BuildArgs {
+           docker: false,
+           elf_name: Some("sp1-helios-elf".to_string()),
+           output_directory: Some("../elf".to_string()),
+           ..Default::default()
+       },
+    );
 }
