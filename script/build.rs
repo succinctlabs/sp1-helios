c9e81c8e48dec1e65e2de9fac6aed3a2cda6911e
nhtyy <n@push0.dev>
2025-06-01 14:26:54 -0700

    chore: bump SP1 v5

--- script/build.rs
+++ script/build.rs
@@ -2,15 +2,14 @@
 use sp1_build::{build_program_with_args, BuildArgs};
 
 fn main() {
-    // build_program_with_args(
-    //    "../program",
-    //    BuildArgs {
-    //        docker: true,
-    //        elf_name: Some("sp1-helios-elf".to_string()),
-    //        tag: Some("v4.1.7".to_string()),
-    //        docker: true,
-    //        output_directory: Some("../elf".to_string()),
-    //        ..Default::default()
-    //    },
-    //);
+    build_program_with_args(
+       "../program",
+       BuildArgs {
+           docker: true,
+           elf_name: Some("sp1-helios-elf".to_string()),
+           tag: "v5.0.0".to_string(),
+           output_directory: Some("../elf".to_string()),
+           ..Default::default()
+       },
+    );
 }
