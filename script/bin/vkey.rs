a4db28453349a895c672c893781731a73a8e8708
leruaa <aurelien.catinon@gmail.com>
2025-09-15 23:58:32 -0700

    chore: remove old ELF

--- script/bin/vkey.rs
+++ script/bin/vkey.rs
@@ -1,11 +1,16 @@
 use anyhow::Result;
 use sp1_sdk::{HashableKey, Prover, ProverClient};
 
-const HELIOS_ELF: &[u8] = include_bytes!("../../elf/sp1-helios-elf");
+const STORAGE_ELF: &[u8] = include_bytes!("../../elf/storage");
+const LIGHT_CLIENT_ELF: &[u8] = include_bytes!("../../elf/light_client");
 
 fn main() -> Result<()> {
     let client = ProverClient::builder().cpu().build();
-    let (_pk, vk) = client.setup(HELIOS_ELF);
-    println!("SP1 Helios Verifying Key: {:?}", vk.bytes32());
+
+    let (_pk, vk) = client.setup(STORAGE_ELF);
+    println!("SP1 Helios Storage Verifying Key: {:?}", vk.bytes32());
+
+    let (_pk, vk) = client.setup(LIGHT_CLIENT_ELF);
+    println!("SP1 Helios Light Client Verifying Key: {:?}", vk.bytes32());
     Ok(())
 }
