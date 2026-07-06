938c64a75dd16e96d85f9f69beb30e14edabcec7
0xernesto <78889960+0xernesto@users.noreply.github.com>
2026-07-06 09:17:42 -0700

    fix: restore sp1-helios ci

---
+++ patches/alloy-signer-aws/src/lib.rs
@@ -0,0 +1,17 @@
+#![recursion_limit = "256"]
+#![doc = include_str!("../README.md")]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/alloy-rs/core/main/assets/alloy.jpg",
+    html_favicon_url = "https://raw.githubusercontent.com/alloy-rs/core/main/assets/favicon.ico"
+)]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+
+#[macro_use]
+extern crate tracing;
+
+mod signer;
+pub use signer::{AwsSigner, AwsSignerError};
+
+pub use aws_config;
+pub use aws_sdk_kms;
